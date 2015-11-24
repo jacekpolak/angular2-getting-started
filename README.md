65e3661171c2107d6593a55041e842e91d08cb17
Jacek Polak <Jacek_Polak@epam.com>
2015-11-24 15:05:32 +0100

    Working on step 2 from tutorial

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # angular2-getting-started
+Project that showing how Angular2 works with TypeScript based on https://angular.io/docs/ts/latest/tutorial/
+
+## Run app
+
+**run watcher for TypeScript**
+
+    npm tsc
+    
+**run live reload server that will open browser and reload on save**
+
+    npm start 
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,69 @@
-import {bootstrap, Component} from 'angular2/angular2';
+import {bootstrap, Component, FORM_DIRECTIVES, NgFor} from 'angular2/angular2';
+
+var HEROES: Hero[] = [
+    { "id": 11, "name": "Mr. Nice"},
+    { "id": 12, "name": "Narco"},
+    { "id": 13, "name": "Some person1"},
+    { "id": 14, "name": "Some person..."},
+    { "id": 15, "name": "Some person..."},
+    { "id": 16, "name": "Some person..."},
+    { "id": 17, "name": "Some person..."},
+    { "id": 18, "name": "Some person..."},
+];
 
 @Component({
-	selector: 'my-app',
-	template: '<h1>My First Angular 2 App</h1>'
+    selector: 'my-app',
+    template: `
+        <h1>{{title}}</h1>
+
+        <h2>My Heroes</h2>
+        <ul class="heroes">
+            <li *ng-for="#hero of heroes">
+                <!-- each hero goes here -->
+                <span class="badge">{{hero.id}}</span> {{hero.name}}
+            </li>
+        </ul>
+
+        <h2>{{hero.name}} details!</h2>
+        <div>
+            <label for="id">id: </label> {{hero.id}}
+        </div>
+        <div>
+            <label for="name">name: </label>
+            <input type="text" id="name" [(ng-model)]="hero.name" placeholder="name" /> 
+        </div>  
+    `,
+    directives: [FORM_DIRECTIVES, NgFor],
+    styles:[`
+        .heroes {list-style-type: none; margin-left: 1em; padding: 0; width: 10em;}
+        .heroes li { cursor: pointer; position: relative; left: 0; transition: all 0.2s ease; }
+        .heroes li:hover {color: #369; background-color: #EEE; left: .2em;}
+        .heroes .badge {
+            font-size: small;
+            color: white;
+            padding: 0.1em 0.7em;
+            background-color: #369;
+            line-height: 1em;
+            position: relative;
+            left: -1px;
+            top: -1px;
+        }
+        .selected { background-color: #EEE; color: #369; }
+    `],
 })
 
-class AppComponent {}
+class AppComponent {
+    public title = 'Tour of Heroes';
+    public hero: Hero = {
+        id: 1,
+        name: 'Windstorm'
+    };
+    public heroes = HEROES;
+}
+
+class Hero {
+    id: number;
+    name: string;
+}
 
 bootstrap(AppComponent);
