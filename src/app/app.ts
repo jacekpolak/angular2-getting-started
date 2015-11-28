6c2c2708efc6c5c532d71d946cfba7f240e37a98
Jacek Polak <jacek.polak@xhtmlized.com>
2015-11-28 09:57:00 +0100

    Add more structure for styles, templates and one reference

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,9 @@
+/// <reference path="libs/jquery.d.ts" />
+
 import {bootstrap, Component, FORM_DIRECTIVES, CORE_DIRECTIVES} from 'angular2/angular2';
 
 var HEROES: Hero[] = [
-    { "id": 11, "name": "Mr. Nice"},
+    { "id": 11, "name": "Mr. Nice Polo"},
     { "id": 12, "name": "Narco"},
     { "id": 13, "name": "Some person1"},
     { "id": 14, "name": "Some person..."},
@@ -13,46 +15,9 @@ var HEROES: Hero[] = [
 
 @Component({
     selector: 'my-app',
-    template: `
-        <h1>{{title}}</h1>
-
-        <h2>My Heroes</h2>
-        <ul class="heroes">
-            <li *ng-for="#hero of heroes" (click)="onSelect(hero)" [ng-class]="getSelectedClass(hero)">
-                <!-- each hero goes here -->
-                <span class="badge">{{hero.id}}</span> {{hero.name}}
-            </li>
-        </ul>
-
-        <div *ng-if="selectedHero">
-            <h2>{{selectedHero.name}} details!</h2>
-            <div>
-                <label for="id">id: </label> {{selectedHero.id}}
-            </div>
-            <div>
-                <label for="name">name: </label>
-                <input type="text" id="name" [(ng-model)]="selectedHero.name" placeholder="name" /> 
-            </div>
-        </div>
-    `,
+    templateUrl: 'app/templates/hero.html',
     directives: [FORM_DIRECTIVES, CORE_DIRECTIVES],
-    styles:[`
-        .heroes { list-style-type: none; margin-left: 1em; padding: 0; width: 10em; }
-        .heroes li { cursor: pointer; position: relative; left: 0; transition: all 0.2s ease; }
-        .heroes li:hover { color: #369; background-color: #EEE; left: .2em;}
-        .heroes .badge {
-            font-size: small;
-            color: white;
-            padding: 0.1em 0.7em;
-            background-color: #369;
-            line-height: 1em;
-            position: relative;
-            left: -1px;
-            top: -1px;
-        }
-        .selected { background-color: #EEE; color: #369; }
-    `]
-    
+    styleUrls: ['app/styles/style.css']
 })
 
 class AppComponent {
